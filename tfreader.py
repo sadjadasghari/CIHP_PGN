dced946da94fddfc209b9c5cf627d148467e3066
sadjadasghari <asgharisajjad@gmail.com>
2019-01-09 18:01:59 -0500

    added tfreader for tfrecords

---
+++ tfreader.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+filenames = tf.placeholder(tf.string, shape=[None])
+dataset = tf.data.TFRecordDataset(filenames)
+dataset = dataset.map(...)  # Parse the record into tensors.
+dataset = dataset.repeat()  # Repeat the input indefinitely.
+dataset = dataset.batch(32)
+iterator = dataset.make_initializable_iterator()
+
+# You can feed the initializer with the appropriate filenames for the current
+# phase of execution, e.g. training vs. validation.
+
+# Initialize `iterator` with training data.
+training_filenames = ["/var/data/file1.tfrecord", "/var/data/file2.tfrecord"]
+sess.run(iterator.initializer, feed_dict={filenames: training_filenames})
+
+# Initialize `iterator` with validation data.
+validation_filenames = ["/var/data/validation1.tfrecord"]
+sess.run(iterator.initializer, feed_dict={filenames: validation_filenames})
